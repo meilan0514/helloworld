fc158f8cb5e644f3174247c85a33f6a33dbedc09
Meilan Chen <meilannihao@gmail.com>
2016-10-05 17:39:41 -0400

    commit lecture note

---
+++ Lecture_notes.R
@@ -0,0 +1,18 @@
+
+
+function(x){
+    if (nclo(x) != 3)
+        stop("x must be 3 column")
+}
+message("success!") # color can turn off
+options(message = FALSE)
+warning() #you have to pay attention, maybe is wrong
+
+# debug the function
+# browse > type n # run the code line by line\
+# type c mean tontinue
+# type ls() list the all the argument, and check each argument, and print each argument to check
+
+
+
+    
